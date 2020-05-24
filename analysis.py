5836c11aaef2276527e07173bcfba40fb81af450
bharurn <bharath.rn@gmail.com>
2020-05-24 20:52:18 +0200

    Multiple trr

--- analysis.py
+++ analysis.py
@@ -1,13 +1,15 @@
 import mimicpy
 from .parse import xvg
+import pandas as pd
 
 class Analysis(mimicpy.core.base.BaseHandle):
-    def gmx(self, cmd, **kwargs):
+    def __init__(self, status=None):
+        super().__init__(status)
+        self.xvg = mimicpy.utils.logger.LogString()
+        self.logger.add(xvg=self.xvg)
+        
+    def _xvg_df(self, cmd, prev_files):
         host = mimicpy.getHost()
-        
-        prev_files = host.ls()
-        
-        super().gmx(cmd, **kwargs)
         
         new_files = host.ls()
         
@@ -15,7 +17,29 @@ class Analysis(mimicpy.core.base.BaseHandle):
         
         if len(extra_files) == 1 and extra_files[0].split('.')[1] == 'xvg':
             o = host.read(extra_files[0])
+            self.logger.write('xvg', f"============Running {cmd}============\n")
+            self.logger.write('xvg', o)
             host.rm(extra_files[0])
             return xvg(o)
         else:
             return extra_files
+    
+    def gmx(self, cmd, **kwargs):
+        
+        prev_files = mimicpy.getHost().ls()
+        
+        if 's' not in kwargs: kwargs['s'] = self.getcurrent('tpr')
+        if 'f' not in kwargs: kwargs['f'] = self.gethistory('trr')
+        
+        _kwargs = kwargs.copy()
+        del _kwargs['f']
+        
+        df = pd.DataFrame()
+        
+        for trr in kwargs['f']:
+            super().gmx(cmd, f=trr, **_kwargs)
+            if df.empty: df = self._xvg_df(cmd, prev_files)
+            else: df = df.append(self._xvg_df(cmd, prev_files))
+        
+        return df.sort_index(axis = 0)
+        
