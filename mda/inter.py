3741c143afae1820439db4462644bed363fb8b48
bharurn <bharath.rn@gmail.com>
2020-05-25 01:45:22 +0200

    Added tqdm loader

--- mda/inter.py
+++ mda/inter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
+from tqdm.notebook import tqdm
 
 def ids_dist(u, sele1, sele2, dist=3):
     mol = u.select_atoms(sele1)
@@ -26,13 +27,22 @@ def run(universe, sele1, sele2):
     _sele2 = []
     time = []
     dist = np.empty(0)
+    
+    t0 = universe.trajectory[0].data['step']
+    step = universe.trajectory[1].data['step'] - t0
+    pbar = tqdm(total=len(universe.trajectory))
+    
+    universe.trajectory[0]
     for t in universe.trajectory:
         a,b,c = ids_dist(universe, sele1, sele2)
         _sele1 += a
         _sele2 += b
         time += [t.data['step']]*len(c)
         dist = np.append(dist, c)
+        pbar.update(1)
     
-    idx = pd.MultiIndex.from_arrays(arrays=[_sele1, _sele2, time], names=[sele1,sele2, "Time"])
+    pbar.close()
+    
+    idx = pd.MultiIndex.from_arrays(arrays=[_sele1, _sele2, time], names=['Sele1','Sele2', "Time"])
     s = pd.Series(data=dist, index=idx)
     return xr.DataArray.from_series(s)
